93c0fa089fbfffcf54aec67ce761ad4da2040622
jo-os <pirtank@gmail.com>
2024-01-27 19:41:48 +0300

    ssh key

--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -36,6 +36,6 @@ variable "vpc_name" {
 
 variable "vms_ssh_root_key" {
   type        = string
-  default     = "<your_ssh_ed25519_key>"
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDNOsMcZ6SDhHfkRbf/64aCLqVhJAmxh5EtXb52TXdTIlyf/NA5rNegRT1v1SJ5z6Me8AvHaZDAhLtvwUDlItjcTnzxCzQSLm07C7szwEgUrHG/++EPc/ZwH026ybep+n+yFmKAoUWq9VsGDhCt4TWu2g3jO9bZTSLTL7tgNDOMuCZq/GsZOj1+vnn0Cyve6gOlUPGOTHD83B7b8iM/lqLaSta2WZc/rbeidEBKUOt1KbmVcN3+l03nebTgKdde3PrQfhP+692DINKb5AJoYE3pw+CZ/AKfqn0sz/FGU2Vn3Kl2w9Uy70y8tiyjaXlBCM5iIx0auYgXTPjc9ywkd/HEKPaPcSknOE9CDrZ1wjO2g9eIAq6VdaudPh52SDdQKDPQ80zMhrQ9ZDFfRb6wrcn2wkUOlknAlI7wCt8r+dbgLgLUz+ObWJ4Uro8ItGyKuTqJt4NizSO2LNu/KcZefx6un6gccIlccXLKJSl3ay5FIlBgshHNjNSNrZ2FzTg/yGU= joos@jooss-MB.local"
   description = "ssh-keygen -t ed25519"
 }
