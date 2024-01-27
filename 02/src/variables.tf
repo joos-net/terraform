1effedf22e5cb7151e6e434ac439170501a4641f
jo-os <pirtank@gmail.com>
2024-01-27 19:46:37 +0300

    some changes

--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -1,16 +1,19 @@
 ###cloud vars
 variable "token" {
   type        = string
+  default = "value"
   description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
 }
 
 variable "cloud_id" {
   type        = string
+  default = "b1gmtujeraulvnf2bj1i"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
 
 variable "folder_id" {
   type        = string
+  default = "b1ghauke2h8p27vt648a"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
