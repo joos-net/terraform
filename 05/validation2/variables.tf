ab5db7cc56f4657141e3fbd88ad9eeff6480d0fd
Jo Os <pirtank@gmail.com>
2024-02-06 09:02:12 +0300

    validation2

--- 05/validation2/variables.tf
+++ 05/validation2/variables.tf
@@ -1,27 +1,27 @@
 variable "low-c" {
   type = string
   description="любая строка"
-  default = "value"
+  default = "min value 312"
   validation {
     error_message = "its not lower case!"
-    condition     = can(regex("^[a-z]+$", var.low-c))
+    condition     = can(regex("^[a-z0-9 ]+$", var.low-c))
   }
 }
 
-# variable "in_the_end_there_can_be_only_one" {
-#     description="Who is better Connor or Duncan?"
-#     type = object({
-#         Dunkan = optional(bool)
-#         Connor = optional(bool)
-#     })
+variable "in_the_end_there_can_be_only_one" {
+    description="Who is better Connor or Duncan?"
+    type = object({
+        Dunkan = optional(bool)
+        Connor = optional(bool)
+    })
 
-#     default = {
-#         Dunkan = true
-#         Connor = false
-#     }
+    default = {
+        Dunkan = true
+        Connor = false
+    }
 
-#     validation {
-#         error_message = "There can be only one MacLeod"
-#         condition = "<проверка>"
-#     }
-# }
+    validation {
+        error_message = "There can be only one MacLeod"
+        condition = var.in_the_end_there_can_be_only_one["Dunkan"] != var.in_the_end_there_can_be_only_one["Connor"]
+    }
+}
